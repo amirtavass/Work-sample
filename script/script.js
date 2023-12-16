f2a91fbd63d0a9beee222d595cfccfb8ef5ccb5d
amirtavass <amirtavass62@gmail.com>
2023-12-16 17:21:17 +0330

    first commit

---
+++ script/script.js
@@ -0,0 +1,165 @@
+let womanProductsData=`[
+{"name":"محصول 1","picUrl":"pics/products/w1.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 2","picUrl":"pics/products/w2.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 3","picUrl":"pics/products/w3.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 4","picUrl":"pics/products/w4.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 5","picUrl":"pics/products/w5.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 6","picUrl":"pics/products/w6.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 7","picUrl":"pics/products/w1.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 8","picUrl":"pics/products/w2.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 9","picUrl":"pics/products/w3.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 10","picUrl":"pics/products/w4.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 11","picUrl":"pics/products/w5.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 12","picUrl":"pics/products/w6.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 13","picUrl":"pics/products/w4.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+{"name":"محصول 14","picUrl":"pics/products/w5.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"}]`;
+
+let manProductsData=`[
+   {"name":"محصول 1","picUrl":"pics/products/m1.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 2","picUrl":"pics/products/m2.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 3","picUrl":"pics/products/m3.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 4","picUrl":"pics/products/m4.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 5","picUrl":"pics/products/m5.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 6","picUrl":"pics/products/m6.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 7","picUrl":"pics/products/m1.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 8","picUrl":"pics/products/m2.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 9","picUrl":"pics/products/m3.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 10","picUrl":"pics/products/m4.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 11","picUrl":"pics/products/m5.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 12","picUrl":"pics/products/m6.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 13","picUrl":"pics/products/m4.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 14","picUrl":"pics/products/m5.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 15","picUrl":"pics/products/m2.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"},
+   {"name":"محصول 16","picUrl":"pics/products/m3.jpg","case":"43میلی متر فولاد ضد زنگ -12 میلی متر ضخامت-مقاومت در برابر اب تا 50 متر","strap":"چرم اصل یکپارچه-دسته فلزی تیتانیوم","glasp":"سگک با دکمه فشاری"}]`;
+
+let productsData="";
+
+
+let asideLinks=document.querySelectorAll("aside a");
+for(let i=0;i<asideLinks.length;i++){
+asideLinks[i].style.transitionDelay=`0.${i*1.2}s`;
+}
+function hahmbergerMenuSwitch(ob){
+ob.classList.toggle("activeHambergerMenu");
+document.querySelector("aside").classList.toggle("openAside");
+
+}
+
+let searchIcon=document.querySelector(".rightHeader .searchSection .search i");
+searchIcon.addEventListener("click",showSearch);
+
+function showSearch(e){
+   let inp=e.currentTarget.parentNode.querySelector("input");
+   if(getComputedStyle(inp).opacity==0){
+   inp.style.display="inline-block";
+   setTimeout(() => inp.style.opacity="1" ,100 ); 
+   }
+   else{
+    inp.style.opacity="0";
+    setTimeout(() => inp.style.display="none" ,500 ); 
+   }
+   }
+
+
+
+   let buttonBoxes=document.querySelectorAll(".buttonBox");
+   for(let x of buttonBoxes){
+      x.addEventListener("click",buttonBoxSelect);
+   }
+
+
+   
+function buttonBoxSelect(e){
+    let main=document.querySelector("main");
+   if(e.currentTarget.classList.contains("buttonBoxMan")){
+    main.classList.toggle("manActive");
+    productsData=JSON.parse(manProductsData);
+
+  
+  }
+  else{
+    main.classList.toggle("womanActive");
+    productsData=JSON.parse(womanProductsData);
+  }
+  if(main.classList.length!=0){
+    // products.innerHTML="";
+     setTimeout(()=>{
+     selectedPage(1);
+ },400);
+  
+  
+  }
+
+else{
+products.innerHTML=""; 
+ }
+
+}
+
+   function selectedPage(page){
+  let products=document.querySelector(".products");
+   let nth=6*(page-1);
+  let pageCount=productsData.length/6;
+  products.innerHTML="<nav></nav>"; 
+  for(let x=5+nth;x>=0+nth;x--){
+  if(x<productsData.length){
+       products.innerHTML=` <div class="product" onclick="showModal(${x})">
+                           <img src="${productsData[x].picUrl}" alt="">
+                          <div class="cover"></div>
+                          <div class="textSection">
+                              <span>مدل:</span>
+                           <span>${productsData[x].name}</span>
+                                </div>
+                              </div> `+products.innerHTML;
+}
+}
+let nav=document.querySelector(".products nav");
+for(let i=0;i<pageCount;i++){
+ nav.innerHTML+=`<span onClick="selectedPage(${i+1})">${i+1}</span>`;
+}
+document.querySelector(`.products nav span:nth-of-type(${page})`).classList.add("navItemActive");
+
+}
+
+function showModal(index){
+   let modal= document.querySelector(".modal");
+   let discriptions=modal.querySelectorAll(".discription");
+   discriptions[0].innerHTML=productsData[index].name;
+   let seperateDiscriptions=String(productsData[index].case).split("-");
+
+   let s="";
+   for(let x of seperateDiscriptions){
+     s+=`<li>${x}</li>`;
+     discriptions[1].innerHTML=s;
+   }
+   seperateDiscriptions=String(productsData[index].strap).split("-");
+
+    s="";
+   for(let x of seperateDiscriptions){
+     s+=`<li>${x}</li>`;
+     discriptions[2].innerHTML=s;
+   }
+   seperateDiscriptions=String(productsData[index].glasp).split("-");
+
+   s="";
+   for(let x of seperateDiscriptions){
+     s+=`<li>${x}</li>`;
+     discriptions[3].innerHTML=s;
+   }
+   modal.querySelector("img").src=productsData[index].picUrl;
+  modal.style.opacity="1";
+  modal.style.display="flex";
+   setTimeout(()=>{
+      document.querySelector(".modal .container").style.opacity="1";
+
+   },500);
+}
+function closeModal(ob){
+  ob.parentNode.parentNode.style.opacity="0";
+  ob.parentNode.style.opacity="0";
+   setTimeout(()=>{
+      ob.parentNode.parentNode.style.display="none";
+
+   },500);
+
+}
